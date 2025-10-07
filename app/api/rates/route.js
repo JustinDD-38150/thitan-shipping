948d03d62e53dfbd1b71ac336ef083b8b7266b01
JustinDD-38150 <justin@cbde.fr>
2025-10-07 12:43:32 +0200

    Update route.js

--- app/api/rates/route.js
+++ app/api/rates/route.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export async function POST(req) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed, use POST' });
   }
